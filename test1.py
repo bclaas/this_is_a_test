a9d085482b6d34c32cd17d8b6228ab6496a71f56
Ben Claas <ben-jaminc@hotmail.com>
2021-10-11 08:50:23 -0500

    Convert test1.py to exercise to practice Git.

--- test1.py
+++ test1.py
@@ -2,14 +2,31 @@
 """
 Created on Thu Jun  3 12:01:10 2021
 
-@author: U282377
+@author: Ben Claas
 """
 
-import numpy as np
-import pandas as pd
+'''
+This is a test file located in a test Git repository.
+If you cloned this repository, it is saved locally - that means
+changes other people make won't appear here. If changes are made
+to this file after cloning/pulling and you want to keep those changes,
+then you must "commit" and "push" them.
+'''
 
-print('This is a test')
+'''
+Type your name below. Then, try to add, commit, and push your changes.
+Remember to "pull" changes before "pushing". Actions should be taken in the order:
+    0.) Save changes to this file (locally)
+    1.) "Add" aka "Stage" changes
+    2.) "Commit" changes (be sure to add a commit message!)
+    3.) "Pull" changes from others
+    3 1/2.) If needed, work through merge conflicts. However, that shouldn't
+            be necessary here.
+    4.) "Push" changes to remote repo
 
+Contributors:
+    1. Ben Claas
 
-my_list=np.ones(5)
-print(my_list)
+'''
+
+print('Hello, World!')
